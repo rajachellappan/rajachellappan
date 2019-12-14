5fd9428ebd9db566850f4b80a8624e33a99a4d75
rajachellappan <36602216+rajachellappan@users.noreply.github.com>
2019-12-14 23:28:26 +0530

    Add files via upload

---
+++ js/dob.js
@@ -0,0 +1,16 @@
+var d = 13;
+var m= 12;
+var y = 1993;
+var cd = new Date().getDay()+ 8;
+var cm= new Date().getMonth()+1;
+var cy = new Date().getFullYear();
+var ad = parseInt(cd)-parseInt(d);
+var am= parseInt(cm)-parseInt(m);
+var ay = parseInt(cy)-parseInt(y);
+
+if (am == 0 && ad > 0){
+		document.write(ay);
+}
+
+else
+		document.write(ay-1);
