65361c746fbcc409c30f9cc613c9ff6ae70aa2ba
rajachellappan <36602216+rajachellappan@users.noreply.github.com>
2019-12-15 17:44:01 +0530

    Update dob.js

--- js/dob.js
+++ js/dob.js
@@ -1,14 +1,14 @@
-var d = 06;
-var m= 02;
-var y = 1993;
-var cd = new Date().getDay()+ 8;
-var cm= new Date().getMonth()+1;
+var d  = 06;
+var m  = 02;
+var y  = 1993;
+var cd = new Date().getDate();
+var cm = new Date().getMonth() + 1;
 var cy = new Date().getFullYear();
 var ad = parseInt(cd)-parseInt(d);
-var am= parseInt(cm)-parseInt(m);
+var am = parseInt(cm)-parseInt(m);
 var ay = parseInt(cy)-parseInt(y);
 
-if (am == 0 && ad > 0){
+if (am > 0||(am == 0 && ad > 0)){
 		document.write(ay);
 }
 
